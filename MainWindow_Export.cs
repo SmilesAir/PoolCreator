a9b45171015df5f740d1eb9a7a8c9c4c90cd63ef
SmilesAir <ryan@smilesair.com>
2020-02-09 21:59:06 -0800

    Save before export. Only export option is to complete judging

--- MainWindow_Export.cs
+++ MainWindow_Export.cs
@@ -10,14 +10,11 @@ using System.Text;
 using System.Windows;
 using System.Windows.Threading;
 using System.Xml.Serialization;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace PoolCreator
 {
 	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
-		Excel.Application excelApp = new Excel.Application();
-
 		HttpClient httpClientDev = new HttpClient();
 		HttpClient httpClientProd = new HttpClient();
 
@@ -29,24 +26,6 @@ namespace PoolCreator
 			{
 				outputText = value;
 				OnPropertyChanged("ExportOutputTextBox");
-			}
-		}
-		public string ExportPath
-		{
-			get { return tournamentData.exportPath; }
-			set
-			{
-				tournamentData.exportPath = value;
-				OnPropertyChanged("ExportPath");
-			}
-		}
-		public string ExcelTemplatePath
-		{
-			get { return tournamentData.excelTemplatePath; }
-			set
-			{
-				tournamentData.excelTemplatePath = value;
-				OnPropertyChanged("ExcelTemplatePath");
 			}
 		}
 
@@ -199,9 +178,6 @@ namespace PoolCreator
 
 		public void InitExport()
 		{
-			ExportPathText.DataContext = this;
-			ExcelTemplatePathText.DataContext = this;
-
 			httpClientDev.DefaultRequestHeaders.Accept.Add(
 			   new MediaTypeWithQualityHeaderValue("application/json"));
 			httpClientProd.DefaultRequestHeaders.Accept.Add(
@@ -213,15 +189,6 @@ namespace PoolCreator
 
 		public void ShutdownExport()
 		{
-			excelApp.Quit();
-		}
-
-		private void ExportExcel_Click(object sender, RoutedEventArgs e)
-		{
-			ExportOutputTextBox = "";
-
-			System.Threading.Thread backgroundThread = new System.Threading.Thread(new System.Threading.ThreadStart(ExportExcelAll));
-			backgroundThread.Start();
 		}
 
 		private void InvokeAppendOutputLine(string line)
@@ -229,343 +196,11 @@ namespace PoolCreator
 			Application.Current.Dispatcher.Invoke(DispatcherPriority.Background,
 				new Action(() => ExportOutputTextBox += line + Environment.NewLine));
 		}
-
-		private void ExportAllPools(Action<PoolKey> exportAction)
-		{
-			for (int divisionIndex = 0; divisionIndex < 4; ++divisionIndex)
-			{
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Prelims, EPool.A));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Prelims, EPool.B));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Prelims, EPool.C));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Prelims, EPool.D));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Quarterfinals, EPool.A));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Quarterfinals, EPool.B));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Quarterfinals, EPool.C));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Quarterfinals, EPool.D));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Semifinals, EPool.A));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Semifinals, EPool.B));
-				exportAction(new PoolKey(EDivision.Open + divisionIndex, ERound.Finals, EPool.A));
-			}
-		}
-
-		private void ExportExcelAll()
-		{
-			InvokeAppendOutputLine("Starting Export");
-
-			ExportAllPools(ExportExcel);
-
-			InvokeAppendOutputLine("Export Finished");
-		}
-
-		private void ExportExcel(PoolKey poolKey)
-		{
-			if (!tournamentData.GetRound(poolKey.division, poolKey.round).shouldExport)
-			{
-				return;
-			}
-
-			try
-			{
-				Excel.Workbook workbook = excelApp.Workbooks.Open(Path.GetFullPath(ExcelTemplatePath), 0, false);
-				Excel.Sheets worksheets = workbook.Worksheets;
-
-				WriteTournamentDetails(poolKey, worksheets);
-				WritePoolDetails(poolKey, worksheets);
-				WriteTeams(poolKey, worksheets);
-				WriteJudges(poolKey, worksheets);
-
-				string newFilename = Path.GetFullPath(ExportPath + @"\" + poolKey.division.ToString() +
-					"-" + GetScoresheetRoundName(poolKey.round) +
-					(poolKey.round != ERound.Finals ? "-" + poolKey.pool.ToString() : "") +
-					".xlsm");
-				File.Delete(newFilename);
-				workbook.SaveAs(newFilename);
-
-				workbook.Close();
-
-				// Process.Start(newFilename);
-
-				InvokeAppendOutputLine("Exported: " + newFilename);
-			}
-			catch (Exception e)
-			{
-				InvokeAppendOutputLine("Error exporting: " + poolKey.division.ToString() + " " + poolKey.round.ToString() +
-					" " + poolKey.pool.ToString() + "  (" + e.Message + ")");
-			}
-		}
-
-		private void WriteTeams(PoolKey poolKey, Excel.Sheets worksheets)
-		{
-			Excel.Worksheet starterSheet = worksheets.get_Item("StarterList");
-			PoolData pd = tournamentData.GetPool(poolKey);
-			if (pd != null && starterSheet != null)
-			{
-				for (int teamIndex = 0; teamIndex < 10 && teamIndex < pd.teamList.teams.Count; ++teamIndex)
-				{
-					TeamData td = pd.teamList.teams[teamIndex];
-					for (int playerIndex = 0; playerIndex < 3 && playerIndex < td.players.Count; ++playerIndex)
-					{
-						starterSheet.Cells[13 + teamIndex, 2 + 2 * playerIndex] = td.players[playerIndex].FullName;
-					}
-				}
-			}
-		}
-
-		private void WriteJudges(PoolKey poolKey, Excel.Sheets worksheets)
-		{
-			Excel.Worksheet starterSheet = worksheets.get_Item("StarterList");
-			PoolData pd = tournamentData.GetPool(poolKey);
-			if (pd != null && starterSheet != null)
-			{
-				for (int exIndex = 0; exIndex < 3 && exIndex < pd.judgesData.judgesEx.Count; ++exIndex)
-				{
-					starterSheet.Cells[13 + exIndex, 10] = pd.judgesData.judgesEx[exIndex].FullName;
-				}
-
-				for (int aiIndex = 0; aiIndex < 3 && aiIndex < pd.judgesData.judgesAi.Count; ++aiIndex)
-				{
-					starterSheet.Cells[16 + aiIndex, 10] = pd.judgesData.judgesAi[aiIndex].FullName;
-				}
-
-				for (int diffIndex = 0; diffIndex < 3 && diffIndex < pd.judgesData.judgesDiff.Count; ++diffIndex)
-				{
-					starterSheet.Cells[19 + diffIndex, 10] = pd.judgesData.judgesDiff[diffIndex].FullName;
-				}
-			}
-		}
-
-		private string GetScoresheetDivisionName(EDivision division)
-		{
-			switch (division)
-			{
-				case EDivision.Open:
-					return "Open Pairs";
-				case EDivision.Mixed:
-					return "Mixed Pairs";
-				case EDivision.Coop:
-					return "Open Coop";
-				case EDivision.Women:
-					return "Women Pairs";
-			}
-
-			return "No Division Name";
-		}
-
-		private string GetScoresheetRoundName(ERound round)
-		{
-			if (round == ERound.Finals)
-			{
-				return "Final";
-			}
-
-			return round.ToString();
-		}
-
-		private void WritePoolDetails(PoolKey poolKey, Excel.Sheets worksheets)
-		{
-			Excel.Worksheet starterSheet = worksheets.get_Item("StarterList");
-			DivisionData dd = tournamentData.GetDivision(poolKey.division);
-			RoundData rd = tournamentData.GetRound(poolKey.division, poolKey.round);
-			if (dd != null && rd != null && starterSheet != null)
-			{
-				starterSheet.Cells[4, 2] = GetScoresheetDivisionName(poolKey.division);
-				starterSheet.Cells[4, 4] = GetScoresheetRoundName(poolKey.round);
-				starterSheet.Cells[4, 6] = poolKey.pool.ToString();
-
-				starterSheet.Cells[3, 10] = rd.scheduleTime.ToShortDateString();
-				starterSheet.Cells[4, 10] = rd.scheduleTime.ToShortTimeString();
-				starterSheet.Cells[5, 10] = rd.routineLength;
-				starterSheet.Cells[6, 10] = dd.headJudge;
-				starterSheet.Cells[7, 10] = dd.directors;
-				starterSheet.Range["J9:J10"].Value = dd.committee;
-			}
-		}
-
-		private void WriteTournamentDetails(PoolKey poolKey, Excel.Sheets worksheets)
-		{
-			Excel.Worksheet settingsSheet = worksheets.get_Item("Settings");
-			settingsSheet.Cells[3, 2] = tournamentData.TournamentName;
-			settingsSheet.Cells[4, 2] = tournamentData.TournamentSubtitle;
-		}
-
-		private void BrowseExportPath_Click(object sender, RoutedEventArgs e)
-		{
-			var dialog = new System.Windows.Forms.FolderBrowserDialog();
-			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-			{
-				ExportPath = dialog.SelectedPath;
-			}
-		}
-
-		private void BrowseExcelTemplate_Click(object sender, RoutedEventArgs e)
-		{
-			Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
-			if (dialog.ShowDialog() == true)
-			{
-				ExcelTemplatePath = dialog.FileName;
-			}
-		}
-
-		private LisaHelperClasses.NameDatabase FillImportedNamesForExport()
-		{
-			LisaHelperClasses.NameDatabase newDatabase = new LisaHelperClasses.NameDatabase(tournamentData);
-
-			TournamentData.importedNames.Clear();
-
-			foreach (LisaHelperClasses.NameData nd in newDatabase.AllNames)
-			{
-				TournamentData.importedNames.Add(new ImportedName(nd.Id, nd.FirstName, nd.LastName));
-			}
-
-			return newDatabase;
-		}
-
-		private void ExportToLisaHelper_Click(object sender, RoutedEventArgs e)
-		{
-			try
-			{
-				XmlSerializer namesSerializer = new XmlSerializer(typeof(LisaHelperClasses.NameDatabase));
-				using (StringWriter newString = new StringWriter())
-				{
-					LisaHelperClasses.NameDatabase nameDatabase = FillImportedNamesForExport();
-
-					namesSerializer.Serialize(newString, nameDatabase);
-
-					string outXml = newString.ToString();
-
-					using (StreamWriter saveFile = new StreamWriter(ExportPath + @"\names.xml",
-						false, System.Text.Encoding.Unicode))
-					{
-						saveFile.Write(outXml);
-					}
-				}
-
-				XmlSerializer saveSerializer = new XmlSerializer(typeof(LisaHelperClasses.TournamentRootData));
-				using (StringWriter newString = new StringWriter())
-				{
-					LisaHelperClasses.TournamentRootData testData = new LisaHelperClasses.TournamentRootData(tournamentData);
-
-					saveSerializer.Serialize(newString, testData);
-
-					string outXml = newString.ToString();
-
-					using (StreamWriter saveFile = new StreamWriter(ExportPath + @"\save.xml",
-						false, System.Text.Encoding.Unicode))
-					{
-						saveFile.Write(outXml);
-					}
-				}
-
-				InvokeAppendOutputLine("Finished Exporting to Lisa Helper");
-			}
-			catch (Exception exception)
-			{
-				InvokeAppendOutputLine("Error Exporting to Lisa Helper: " + exception.Message);
-			}
-		}
-
-		private void ExportToPotlatch_Click(object sender, RoutedEventArgs e)
-		{
-			string saveFilename = ExportPath + @"\PotlatchJudgerNames.txt";
-			try
-			{
-				using (StreamWriter saveFile = new StreamWriter(saveFilename))
-				{
-					ExportPotlatchPool(saveFile, new PoolKey(EDivision.Coop, ERound.Finals, EPool.A));
-				}
-			}
-			catch (Exception exception)
-			{
-				InvokeAppendOutputLine("Error Exporting to Potlatch Judger: " + exception.Message);
-			}
-		}
-
-		private void ExportPotlatchPool(StreamWriter stream, PoolKey poolKey)
-		{
-			stream.WriteLine("Division: " + poolKey.division + " Round: " + poolKey.round + " Pool: " + poolKey.pool);
-
-			PoolData pd = tournamentData.GetPool(poolKey);
-			for (int teamIndex = 0; teamIndex < 10 && teamIndex < pd.teamList.teams.Count; ++teamIndex)
-			{
-				TeamData td = pd.teamList.teams[teamIndex];
-				stream.WriteLine(td.PlayerNames);
-			}
-
-			stream.WriteLine();
-		}
-
-		private void ExportDialJudgerImportText(PoolKey poolKey)
-		{
-			if (!tournamentData.GetRound(poolKey.division, poolKey.round).shouldExport)
-			{
-				return;
-			}
-
-			PoolData pd = tournamentData.GetPool(poolKey);
-			if (pd.teamList.teams.Count == 0 && !pd.judgesData.HasJudges())
-			{
-				return;
-			}
-
-			string outputText = "";
-			string exportName = poolKey.division.ToString() + "-" +
-				poolKey.round.ToString() + "-" +
-				poolKey.pool.ToString();
-
-			outputText += Environment.NewLine;
-			outputText += "///////////////////////////////////////" + Environment.NewLine;
-			outputText += "// " + exportName + Environment.NewLine;
-
-			foreach (TeamData td in pd.teamList.teams)
-			{
-				outputText += td.PlayerNamesCommaSeparated + Environment.NewLine;
-			}
-
-			outputText += Environment.NewLine;
-			outputText += "// Judges" + Environment.NewLine;
-
-			foreach (RegisteredPlayer rp in pd.judgesData.judgesAi)
-			{
-				outputText += rp.FullName + ", ArtisticImpression" + Environment.NewLine;
-			}
-			foreach (RegisteredPlayer rp in pd.judgesData.judgesDiff)
-			{
-				outputText += rp.FullName + ", Difficulty" + Environment.NewLine;
-			}
-			foreach (RegisteredPlayer rp in pd.judgesData.judgesEx)
-			{
-				outputText += rp.FullName + ", Execution" + Environment.NewLine;
-			}
-
-			outputText += "///////////////////////////////////////" + Environment.NewLine;
-			outputText += Environment.NewLine;
-
-			ExportOutputTextBox += outputText;
-
-			try
-			{
-				using (StreamWriter saveFile = new StreamWriter(ExportPath + @"\" + exportName + ".txt",
-						false, System.Text.Encoding.Unicode))
-				{
-					saveFile.Write(outputText);
-				}
-			}
-			catch
-			{
-			}
-		}
-
-		private void ExportDialJudger_Click(object sender, RoutedEventArgs e)
-		{
-			ExportOutputTextBox = "------------ Starting Generation ------------" + Environment.NewLine;
-
-			ExportAllPools(ExportDialJudgerImportText);
-
-			ExportOutputTextBox += "------------ Finished Generation ------------";
-		}
-
+		
 		private void ExportCompleteJudging_Click(object sender, RoutedEventArgs e)
 		{
+			tournamentData.SaveToDisk();
+
 			ExportToJson();
 		}
 
@@ -584,6 +219,8 @@ namespace PoolCreator
 			InvokeAppendOutputLine("Uploaded to development at " + DateTime.Now.ToString());
 
 			OutputLinks(false);
+
+			InvokeAppendOutputLine("Finished");
 		}
 
 		private void UploadProdCompleteJudging_Click(object sender, RoutedEventArgs e)
@@ -601,6 +238,8 @@ namespace PoolCreator
 			InvokeAppendOutputLine("Uploaded to PRODUCTION at " + DateTime.Now.ToString());
 
 			OutputLinks(true);
+
+			InvokeAppendOutputLine("Finished");
 		}
 
 		private void OutputLinks(bool isProd)
